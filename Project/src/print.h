254587f33ac202a1fb5464a6e2f29e16277f1b6f
jesseboth <jessebot@buffalo.edu>
2022-06-30 17:10:28 +0000

    adjusted to cast as the type to be printed

--- Project/src/print.h
+++ Project/src/print.h
@@ -1,7 +1,9 @@
-#define dprint(expr) printf(#expr ": %d\n", expr)
+#include <stdio.h>
+
+#define dprint(expr) printf(#expr ": %d\n", (int)expr)
 #define pprint(expr) printf(#expr ": %p\n", expr)
-#define fprint(expr) printf(#expr ": %f\n", expr)
-#define sprint(expr) printf(#expr ": %s\n", expr)
-#define cprint(expr) printf(#expr ": %c\n", expr)
+#define fprint(expr) printf(#expr ": %f\n", (float)expr)
+#define sprint(expr) printf(#expr ": %s\n", (char *)expr)
+#define cprint(expr) printf(#expr ": %c\n", (char)expr)
 #define hprint(expr) printf(#expr ": 0x%x\n", expr)
 
